0b06546d3e5dc308725337dc0f1e85f738dc803f
Timothy Johnston <timothy.johnston@bnymellon.com>
2018-11-16 11:43:11 -0500

    starting JPA implementation

--- src/main/java/com/tj/Application.java
+++ src/main/java/com/tj/Application.java
@@ -3,7 +3,9 @@ package com.tj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @ComponentScan("com.tj")
@@ -12,6 +14,12 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 		System.out.println("hi");
+	}
+	
+	@Profile("prod")
+	@Bean
+	public String dummy() {
+		return "something!!!";
 	}
 
 }
---
+++ src/main/java/com/tj/jpa/User.java
@@ -0,0 +1,18 @@
+package com.tj.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String role;
+	
+}
