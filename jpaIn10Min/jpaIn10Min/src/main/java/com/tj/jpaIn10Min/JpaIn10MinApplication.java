22819b30bf4b4e8068e2aad2dd070469b7e0b581
Timothy Johnston <tedwardj11@gmail.com>
2018-12-12 13:51:57 -0500

    initialized japIn10Min project

---
+++ jpaIn10Min/jpaIn10Min/src/main/java/com/tj/jpaIn10Min/JpaIn10MinApplication.java
@@ -0,0 +1,14 @@
+package com.tj.jpaIn10Min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIn10MinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaIn10MinApplication.class, args);
+	}
+
+}
+
