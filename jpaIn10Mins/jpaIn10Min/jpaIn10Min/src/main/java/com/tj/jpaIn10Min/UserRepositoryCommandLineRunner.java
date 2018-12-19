cec4789e02947cbc362c652448f043b27eda1ea0
Timothy Johnston <tedwardj11@gmail.com>
2018-12-19 14:59:51 -0500

    added findAll to userrepositorycommandlinerunner and started test cases

--- jpaIn10Mins/jpaIn10Min/jpaIn10Min/src/main/java/com/tj/jpaIn10Min/UserRepositoryCommandLineRunner.java
+++ jpaIn10Mins/jpaIn10Min/jpaIn10Min/src/main/java/com/tj/jpaIn10Min/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,9 @@
 package com.tj.jpaIn10Min;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +28,9 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 		//New user is created
 		userRepository.save(user);
 		log.info("New User is created : " + user);
+		
+		ArrayList<User> users = (ArrayList<User>) userRepository.findAll();
+		log.info("User is retrived: " + users);
 	}
 	
 }
