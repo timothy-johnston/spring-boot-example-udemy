76f2ae7a57eb67ded3cedc1c7a70a496b0ff1e3c
Timothy Johnston <tedwardj11@gmail.com>
2018-12-20 15:02:52 -0500

    created springIn5Steps maven project

---
+++ springIn5Steps/src/main/java/com/tj/springIn5Steps/App.java
@@ -0,0 +1,13 @@
+package com.tj.springIn5Steps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
